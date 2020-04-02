fc795481156a2fb9787e2a369534c0f30c17ca24
Stefane Fivaz <s.a.fivaz@gmail.com>
2020-04-03 00:38:55 +0200

    include

---
+++ src/main/java/ch/hesge/programmation/Initial2Servlet.java
@@ -0,0 +1,23 @@
+package ch.hesge.programmation;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/initial2")
+public class Initial2Servlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html><body><h1>Test initial 2</h1></body></html>");
+        RequestDispatcher dispatcher = getServletContext()
+                .getRequestDispatcher("/include");
+        dispatcher.include(req, resp);
+    }
+}
--- src/main/java/ch/hesge/programmation/Redirect2Servlet.java
+++ src/main/java/ch/hesge/programmation/Redirect2Servlet.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns = "/redirect2")
-public class RedirectServlet2 extends HttpServlet {
+public class Redirect2Servlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
