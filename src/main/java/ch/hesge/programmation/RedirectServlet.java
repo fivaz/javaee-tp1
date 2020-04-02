b25f43d5f74885c86caa71e7eba72ac79d21d0f9
Stefane Fivaz <s.a.fivaz@gmail.com>
2020-04-03 00:12:57 +0200

    redirect

---
+++ src/main/java/ch/hesge/programmation/RedirectServlet.java
@@ -0,0 +1,16 @@
+package ch.hesge.programmation;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/redirect")
+public class RedirectServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.sendRedirect("https://google.com");
+    }
+}
