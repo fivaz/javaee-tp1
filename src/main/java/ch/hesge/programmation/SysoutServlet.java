b11d2a7b621fbd680456187a2da545c4e43ae661
Stefane Fivaz <s.a.fivaz@gmail.com>
2020-04-03 00:08:35 +0200

    routing with web.xml

--- src/main/java/ch/hesge/programmation/SysoutServlet.java
+++ src/main/java/ch/hesge/programmation/SysoutServlet.java
@@ -1,13 +1,14 @@
+package ch.hesge.programmation;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/sysout")
 public class SysoutServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
-        System.out.println("test");
+        System.out.println("test1");
     }
 }
