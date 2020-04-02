0194ec5be651cf77bd63bbc0f588f7e13c84ea72
Stefane Fivaz <s.a.fivaz@gmail.com>
2020-04-03 01:03:24 +0200

    fixing exercise 3.5

--- src/main/java/ch/hesge/programmation/ParamServlet.java
+++ src/main/java/ch/hesge/programmation/ParamServlet.java
@@ -4,14 +4,22 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.Map;
 
 @WebServlet(urlPatterns = "/param")
 public class ParamServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         Map<String, String[]> parameterMap = req.getParameterMap();
-        System.out.println(parameterMap);
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html><body><table border=2>");
+        for (String param : parameterMap.keySet()) {
+            writer.println("<tr><td>" + param + "</td><td>" + Arrays.toString(parameterMap.get(param)) + "</td></tr>");
+        }
+        writer.println("</table></body></html>");
     }
 }
