97fc3ce8c19a8839d7b62f923288174978c48fa6
Stefane Fivaz <s.a.fivaz@gmail.com>
2020-04-03 00:35:26 +0200

    forward

---
+++ src/main/java/ch/hesge/programmation/ForwardServlet.java
@@ -0,0 +1,15 @@
+package ch.hesge.programmation;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/forward")
+public class ForwardServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        System.out.println("forward");
+    }
+}
---
+++ src/main/java/ch/hesge/programmation/InitialServlet.java
@@ -0,0 +1,23 @@
+package ch.hesge.programmation;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/initial")
+public class InitialServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher dispatcher = getServletContext()
+                .getRequestDispatcher("/forward");
+        dispatcher.forward(req, resp);
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html><body><h1>Test forwarded</h1></body></html>");
+    }
+}
