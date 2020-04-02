bcb7da1220caa7df122153c9af58b0fd68515e2e
Stefane Fivaz <s.a.fivaz@gmail.com>
2020-04-03 00:17:38 +0200

    cleanup

--- src/main/java/ch/hesge/programmation/RedirectServlet2.java
+++ src/main/java/ch/hesge/programmation/RedirectServlet2.java
@@ -4,13 +4,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @WebServlet(urlPatterns = "/redirect2")
 public class RedirectServlet2 extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
         resp.setHeader("Location", "http://www.google.com");
         resp.setStatus(HttpServletResponse.SC_FOUND);
     }
