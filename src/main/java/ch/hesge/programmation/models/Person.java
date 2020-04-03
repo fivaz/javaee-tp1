b333528eb0a8eda6fe6228b6805b89e8bff8405e
Stefane Fivaz <s.a.fivaz@gmail.com>
2020-04-03 09:00:30 +0200

    5

---
+++ src/main/java/ch/hesge/programmation/models/Person.java
@@ -0,0 +1,16 @@
+package ch.hesge.programmation.models;
+
+public class Person {
+    public final String firstName;
+    public final String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName;
+    }
+}
